0fb32ec7a421933c8c948e5c62331739487a2697
Zarah08 <zarahabubakar1234@gmail.com>
2026-02-11 13:29:44 +0100

    creating role based login

---
+++ src/users.js
@@ -0,0 +1,7 @@
+// src/users.js
+export const users = [
+    { id: 1, username: "admin", password: "admin123", role: "admin" },
+    { id: 2, username: "teacher1", password: "teacher123", role: "teacher" },
+    { id: 3, username: "student1", password: "student123", role: "student" },
+  ];
+  
